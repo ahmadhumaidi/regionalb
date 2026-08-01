d7bf7e6d5c9bced14b5fb8acc236342f9f48ca38
Ahmad Humaidi <ahumaidi35@gmail.com>
2026-08-01 12:19:32 +0000

    Stop tracking runtime uploads and source_activity in git

--- source_activity/meta.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'app_id' => 'ISI_APP_ID_META',
-    'app_secret' => 'ISI_APP_SECRET_META',
-    'redirect_uri' => 'https://regionalb.online/instagram-callback.php',
-    'graph_version' => 'v20.0',
-];
--- source_activity/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'app_id' => '1669323834145995',
-    'app_secret' => '28a1f8c5a6973ef90b217f8c8c79b7c8',
-    'redirect_uri' => 'https://regionalb.online/instagram-callback.php',
-    'graph_version' => 'v20.0',
-];
